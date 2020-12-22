3cbd3bd786d362268fdef0deaa95ff181b04dcf1
Francerz <francerzam@hotmail.com>
2020-12-22 11:26:47 -0600

    Added other Token request Grant Types

--- src/TokenRequestGrantTypes.php
+++ src/TokenRequestGrantTypes.php
@@ -4,6 +4,10 @@ namespace Francerz\OAuth2;
 
 class TokenRequestGrantTypes
 {
-    const AUTHORIZATION_CODE = 'authorization_code';
-    const REFRESH_TOKEN = 'refresh_token';
+    const AUTHORIZATION_CODE    = 'authorization_code';
+    const TOKEN                 = 'token';
+    const PASSWORD              = 'password';
+    const CLIENT_CREDENTIALS    = 'client_credentials';
+
+    const REFRESH_TOKEN         = 'refresh_token';
 }
