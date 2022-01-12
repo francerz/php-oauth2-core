f0e8f4fa9f7fbb1f5d66d6746d2dbbbc29398262
Francerz <francerzam@hotmail.com>
2022-01-12 12:39:18 -0600

    IMP PKCEHelper::generateCode() is URL safe.

--- src/PKCEHelper.php
+++ src/PKCEHelper.php
@@ -3,15 +3,20 @@
 namespace Francerz\OAuth2;
 
 use Francerz\Http\Utils\UriHelper;
+use RuntimeException;
 
 abstract class PKCEHelper
 {
     /**
-     * @param int $length
+     * Generates a criptographically random string with URL safe features.
+     * 
+     * @param int $length The length of random string.
      */
-    public static function generateCode($length)
+    public static function generateCode($length = 32)
     {
-        return random_bytes(ceil($length * 3 / 4));
+        $string = random_bytes(ceil($length * 3 / 4));
+        $urlsafe = strtr(rtrim(base64_encode($string), '='), '+/', '-_');
+        return substr($urlsafe, 0, $length);
     }
     public static function urlEncode($code, $method = CodeChallengeMethodsEnum::PLAIN)
     {
@@ -20,6 +25,8 @@ abstract class PKCEHelper
                 return $code;
             case CodeChallengeMethodsEnum::SHA256:
                 return UriHelper::base64Encode(hash('sha256', $code, true));
+            default:
+                throw new RuntimeException('Unknown Code Challenge Method.');
         }
     }
 }
